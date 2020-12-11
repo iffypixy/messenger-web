7438677125b789f6ddd57299e3e6726f5ec3090f
Ansat Jartybayev <lud1crous@mail.ru>
2020-12-11 19:41:59 +0600

    feat(app): added server for static

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,27 @@
 import express from "express";
 import {createProxyMiddleware} from "http-proxy-middleware";
+import dotenv from "dotenv";
 import next from "next";
+
+dotenv.config();
 
 const dev = process.env.NODE_ENV !== "production";
 
 const app = next({dev});
 
+const handler = app.getRequestHandler();
+
 app.prepare().then(() => {
   const server = express();
-
-  const handler = app.getRequestHandler();
 
   server
     .disable("x-powered-by")
     .use("/api", createProxyMiddleware({
       target: process.env.BACKEND_URL,
       pathRewrite: {
-        "^/api": "",
+        "^/api": ""
       },
-      secure: false,
+      secure: false
     }))
     .all("*", (req, res) => handler(req, res));
 
