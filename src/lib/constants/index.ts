c71b2aa20f75a81eead40082ec1fcaeb40dd9dec
Ansat Jartybayev <ansatjr@gmail.com>
2021-07-12 00:43:41 +0600

    fix: env & gitignore

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "https://api.ansatjr.xyz";
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL!;
--- src/lib/websocket/socket.ts
+++ src/lib/websocket/socket.ts
@@ -2,6 +2,7 @@ import {io} from "socket.io-client";
 
 import {BACKEND_URL} from "@lib/constants";
 
-export const socket = io(BACKEND_URL, {
-  withCredentials: true
+export let socket = io(BACKEND_URL, {
+  withCredentials: true,
+  autoConnect: false
 });
