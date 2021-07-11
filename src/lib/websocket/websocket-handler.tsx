8910cdeb1193adcaff34185b11d188d3566a2516
Ansat Jartybayev <ansatjr@gmail.com>
2021-07-11 16:36:07 +0600

    fix: websocket-handler

--- src/lib/websocket/websocket-handler.tsx
+++ src/lib/websocket/websocket-handler.tsx
@@ -10,42 +10,51 @@ import {socket} from "./socket";
 export const WebsocketHandler: React.FC = ({children}) => {
   const dispatch = useRootDispatch();
 
-  const credentials = useSelector(authSelectors.credentials);
+  const credentials = useSelector(authSelectors.credentials)!;
 
   useEffect(() => {
-    if (!!credentials) {
-      socket.on("DIRECT_CHAT:MESSAGE", ({message, partner}: {message: DirectChatMessage; partner: DirectChatPartner}) => {
-        dispatch(directsActions.addMessage({
+    socket.connect();
+  }, [credentials]);
+
+  useEffect(() => {
+    socket.on("DIRECT_CHAT:MESSAGE", ({message, partner}: {message: DirectChatMessage; partner: DirectChatPartner}) => {
+      dispatch(directsActions.addMessage({
+        message, isOwn: false,
+        partnerId: partner.id
+      }));
+    });
+
+    socket.on("GROUP_CHAT:MESSAGE", ({message, chat}: {message: GroupChatMessage; chat: GroupChatDetails}) => {
+      const isOwn = (message.sender && message.sender.id) === credentials.id;
+
+      if (!isOwn) {
+        dispatch(groupsActions.addMessage({
           message, isOwn: false,
-          partnerId: partner.id
+          groupId: chat.id
         }));
-      });
+      }
+    });
 
-      socket.on("GROUP_CHAT:MESSAGE", ({message, chat}: {message: GroupChatMessage; chat: GroupChatDetails}) => {
-        const isOwn = (message.sender && message.sender.id) === credentials.id;
+    socket.on("DIRECT_CHAT:MESSAGE_READ", ({message, partner}: {message: DirectChatMessage; partner: DirectChatPartner}) => {
+      dispatch(directsActions.readMessage({
+        partnerId: partner.id,
+        messageId: message.id
+      }));
+    });
 
-        if (!isOwn) {
-          dispatch(groupsActions.addMessage({
-            message, isOwn: false,
-            groupId: chat.id
-          }));
-        }
-      });
+    socket.on("GROUP_CHAT:MESSAGE_READ", ({message, chat}: {message: GroupChatMessage; chat: GroupChatDetails}) => {
+      dispatch(groupsActions.readMessage({
+        groupId: chat.id,
+        messageId: message.id
+      }));
+    });
 
-      socket.on("DIRECT_CHAT:MESSAGE_READ", ({message, partner}: {message: DirectChatMessage; partner: DirectChatPartner}) => {
-        dispatch(directsActions.readMessage({
-          partnerId: partner.id,
-          messageId: message.id
-        }));
-      });
-
-      socket.on("GROUP_CHAT:MESSAGE_READ", ({message, chat}: {message: GroupChatMessage; chat: GroupChatDetails}) => {
-        dispatch(groupsActions.readMessage({
-          groupId: chat.id,
-          messageId: message.id
-        }));
-      });
-    }
+    return () => {
+      socket.off("DIRECT_CHAT:MESSAGE");
+      socket.off("DIRECT_CHAT:MESSAGE_READ");
+      socket.off("GROUP_CHAT:MESSAGE");
+      socket.off("GROUP_CHAT:MESSAGE_READ");
+    };
   }, []);
 
   return children as ReactElement;
